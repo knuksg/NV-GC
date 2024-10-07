9aadc9e266f2a33b9586192c3170cea15119c4cb
knuksg <knuksg@naver.com>
2024-10-07 15:41:54 +0900

    init

---
+++ geocode.js
@@ -0,0 +1,34 @@
+// api/geocode.js
+
+import axios from "axios";
+
+export default async (req, res) => {
+    const address = req.query.address;
+    if (!address) {
+        return res.status(400).json({ error: "주소를 입력해주세요." });
+    }
+
+    const client_id = process.env.NAVER_CLIENT_ID;
+    const client_secret = process.env.NAVER_CLIENT_SECRET;
+
+    const url = `https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=${encodeURIComponent(address)}`;
+
+    try {
+        const response = await axios.get(url, {
+            headers: {
+                "X-NCP-APIGW-API-KEY-ID": client_id,
+                "X-NCP-APIGW-API-KEY": client_secret,
+            },
+        });
+
+        if (response.data.addresses.length > 0) {
+            const { x: longitude, y: latitude } = response.data.addresses[0];
+            res.json({ latitude, longitude });
+        } else {
+            res.status(404).json({ error: "좌표를 찾을 수 없습니다." });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "서버 오류가 발생했습니다." });
+    }
+};
